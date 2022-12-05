93d0f14b936ce20e91b1040751fb69557c34fd18
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-05 20:17:24 +0100

    Day 3.1

---
+++ Advent/Advent/Assignments/Day03_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Assignments
+{
+    internal class Day03_1 : IAssignment
+    {
+        public Task<string> RunAsync(IReadOnlyList<string> input, CancellationToken cancellationToken = default)
+        {
+            var sum = 0;
+
+            var addedItems = new HashSet<char>();
+
+            foreach (var line in input)
+            {
+                var firstCompartment = line[..(line.Length / 2)];
+                var secondCompartment = line[(line.Length / 2)..];
+                
+                foreach (var item in secondCompartment)
+                {
+                    if (!addedItems.Contains(item) && firstCompartment.Contains(item))
+                    {
+                        sum += GetPriority(item);
+                        addedItems.Add(item);
+                    }
+                }
+
+                addedItems.Clear();
+            }
+
+            return Task.FromResult(sum.ToString());
+        }
+
+        private static int GetPriority(char item)
+        {
+            if (item >= 'a' && item <= 'z')
+                return item - 'a' + 1;
+            else if (item >= 'A' && item <= 'Z')
+                return item - 'A' + 27;
+            throw new ArgumentException(null, nameof(item));
+        }
+    }
+}
---
+++ Advent/Advent/Assignments/Day03_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Assignments
+{
+    internal class Day03_2 : IAssignment
+    {
+        public Task<string> RunAsync(IReadOnlyList<string> input, CancellationToken cancellationToken = default)
+        {
+            var sum = 0;
+
+
+            return Task.FromResult(sum.ToString());
+        }
+    }
+}
