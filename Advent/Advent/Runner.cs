68f98e37cea93c8a8d8fdac6a61d6931bccbe9f1
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-06 19:38:56 +0100

    Add timing info to log output

--- Advent/Advent/Runner.cs
+++ Advent/Advent/Runner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace Advent
 
         public async Task RunAsync(CancellationToken cancellationToken)
         {
+            var stopwatch = new Stopwatch();
+
             foreach (var assingment in _assignments)
             {
                 if (cancellationToken.IsCancellationRequested)
@@ -27,13 +30,17 @@ namespace Advent
                     var lines = await File.ReadAllLinesAsync(inputName, cancellationToken).ConfigureAwait(false);
 
                     Logger.Line($"Running assignment {assingment.Name}");
+                    stopwatch.Restart();
                     var result = await assingment.RunAsync(lines, cancellationToken).ConfigureAwait(false);
-
+                    stopwatch.Stop();
                     Logger.Line($"Result of {assingment.Name}: {result}");
+                    Logger.Line($"Took {stopwatch.ElapsedMilliseconds} ms");
                 }
                 catch(Exception ex) 
                 {
+                    stopwatch.Stop();
                     Logger.ErrorLine($"Exception when running assignment {assingment.Name}: {ex.Message}\n{ex.StackTrace}");
+                    Logger.ErrorLine($"Took {stopwatch.ElapsedMilliseconds} ms");
                 }
 
                 Logger.Line();
