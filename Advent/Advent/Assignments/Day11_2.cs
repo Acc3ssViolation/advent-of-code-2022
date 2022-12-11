c003b7da4da15359c175bcc95b160ce5c26288f9
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-11 18:02:32 +0100

    Slightly faster

--- Advent/Advent/Assignments/Day11_2.cs
+++ Advent/Advent/Assignments/Day11_2.cs
@@ -1,45 +1,119 @@
-﻿//#define LOG_MONKEYS
-//#define LOG_ROUNDS
+﻿//#define LOG_ROUNDS
 
 namespace Advent.Assignments
 {
     internal class Day11_2 : IAssignment
     {
+        enum MonkeyOp
+        {
+            Add,
+            Multiply,
+            Square,
+        }
+
+        struct Monkey
+        {
+            public MonkeyOp op;
+            public int opValue;
+            public int test;
+            public int nextIndexTrue;
+            public int nextIndexFalse;
+            public int items;
+            public int inspected;
+        }
+
         public string Run(IReadOnlyList<string> input)
         {
-            var monkeys = new List<Monkey2>();
-            var builder = new MonkeyBuilder2();
+            const int MaxMonkeys = 8;
+            const int MaxItems = 64;
+            var normalizer = 1;
+
+            var items = new int[MaxMonkeys * MaxItems];
+            var monkeys = new Monkey[MaxMonkeys];
+            var monkeyCount = 0;
 
             for (int i = 0; i < input.Count; i++)
             {
                 if (input[i].Length > 0 && input[i][0] == 'M')
                 {
-                    builder.ParseStartingItems(input[i + 1]);
-                    builder.ParseOperation(input[i + 2]);
-                    // The divisor here is always prime! This means that for the Test to be true, the worry level is NOT prime
-                    builder.ParseTest(input[i + 3], input[i + 4], input[i + 5]);
-                    monkeys.Add(builder.Build());
+                    ref var monkey = ref monkeys[monkeyCount];
+
+                    // Load the starting items
+                    var split = input[i + 1][18..].Split(',');
+                    for (int k = 0; k < split.Length; k++)
+                        items[monkeyCount * MaxItems + k] = int.Parse(split[k]);
+                    monkey.items = split.Length;
+
+                    // Load the operation
+                    monkey.op = input[i + 2][23] == '+' ? MonkeyOp.Add : MonkeyOp.Multiply;
+                    var operand = input[i + 2].AsSpan(25);
+                    if (operand[0] == 'o')
+                        monkey.op = MonkeyOp.Square;
+                    else
+                        monkey.opValue = int.Parse(operand);
+
+                    // Load the test
+                    monkey.test = int.Parse(input[i + 3].AsSpan(21));
+                    monkey.nextIndexTrue = int.Parse(input[i + 4].AsSpan(29));
+                    monkey.nextIndexFalse = int.Parse(input[i + 5].AsSpan(30));
+
+                    // The normalizer must be a common multiple of all test divisors. Since they are all prime this multiplication will give us the Least Common Multiple.
+                    normalizer *= monkey.test;
+
+                    i += 6;
+                    monkeyCount++;
                 }
             }
 
-            for (int i = 0; i < monkeys.Count; i++)
-                monkeys[i].Divisor = builder.Divisor;
-
-            for (int i = 0; i < 10000; i++)
+            for (int r = 0; r < 10000; r++)
             {
-                for (int m = 0; m < monkeys.Count; m++)
-                    monkeys[m].Round(monkeys);
+                for (int m = 0; m < monkeyCount; m++)
+                {
+                    // Monkey businessssss
+                    ref var monkey = ref monkeys[m];
+                    monkey.inspected += monkey.items;
+
+                    var itemSlice = items.AsSpan(m * MaxItems, MaxItems);
+                    for (int i = 0; i < monkey.items; i++)
+                    {
+                        long worry = itemSlice[i];
+                        if (monkey.op == MonkeyOp.Add)
+                            worry += monkey.opValue;
+                        else if (monkey.op == MonkeyOp.Multiply)
+                            worry *= monkey.opValue;
+                        else
+                            worry *= worry;
+
+                        // Todo: use bitmask if possible?
+                        worry %= normalizer;
+                        var target = monkey.nextIndexFalse;
+                        if (worry % monkey.test == 0)
+                            target = monkey.nextIndexTrue;
+                        // Move the item to the other monkey
+                        items[target * MaxItems + monkeys[target].items] = (int)worry;
+                        monkeys[target].items++;
+                    }
+                    monkey.items = 0;
+                }
+
 #if LOG_ROUNDS
-                LogRound(monkeys, i + 1);
+                // PRINT IT
+                if (r == 0 || r == 19 || (r % 1000 == 999))
+                {
+                    Logger.DebugLine($"== After round {r + 1} ==");
+                    for (int m = 0; m < monkeyCount; m++)
+                        Logger.DebugLine($"Monkey {m} inspected items {monkeys[m].inspected} times.");
+                    Logger.Line();
+                }
 #endif
             }
 
             long max1 = 0;
             long max2 = 0;
 
-            for (int i = 0; i < monkeys.Count; i++)
+            for (int i = 0; i < monkeyCount; i++)
             {
-                var itemCount = monkeys[i].InspectedItems;
+                var itemCount = monkeys[i].inspected;
                 if (itemCount > max1)
                 {
                     max2 = max1;
@@ -54,145 +128,6 @@ namespace Advent.Assignments
             var business = max1 * max2;
 
             return business.ToString();
-        }
-
-        private static void LogRound(List<Monkey2> monkeys, int round)
-        {
-            Logger.DebugLine($"After round {round}, the monkeys are holding items with these worry levels:");
-            for (int i = 0; i < monkeys.Count;i++)
-            {
-                Logger.DebugLine($"Monkey {i}: {monkeys[i].Items.Aggregate("", (s, i) => (s == "" ? i.ToString() : $"{s}, {i}"))}");
-            }
-        }
-    }
-
-    internal class Monkey2
-    {
-        private readonly List<long> _items = new();
-        private bool _addition;
-        private int _operationValue;
-        private int _testDivisor;
-        private int _nextIndexTrue;
-        private int _nextIndexFalse;
-        private int _inspectedItems;
-        private int _divisor;
-
-        public int Divisor { get { return _divisor; } set { _divisor = value; } }
-        public IReadOnlyList<long> Items => _items;
-        public int InspectedItems => _inspectedItems;
-
-        public Monkey2(List<long> items, bool addition, int operationValue, int testDivisor, int nextIndexTrue, int nextIndexFalse)
-        {
-            _items = items ?? throw new ArgumentNullException(nameof(items));
-            _addition = addition;
-            _operationValue = operationValue;
-            _testDivisor = testDivisor;
-            _nextIndexTrue = nextIndexTrue;
-            _nextIndexFalse = nextIndexFalse;
-        }
-
-        public void Round(List<Monkey2> monkeys)
-        {
-#if LOG_MONKEYS
-            Logger.DebugLine($"Monkey {monkeys.IndexOf(this)}:");
-#endif
-            for (int i = 0; i < _items.Count; i++)
-            {
-                checked
-                {
-                    _inspectedItems++;
-                    var worryLevel = _items[i];
-#if LOG_MONKEYS
-                Logger.DebugLine($"\tMonkey inspects an item with a worry level of {worryLevel}");
-#endif
-                    var operand = _operationValue == -1 ? worryLevel : _operationValue;
-                    if (_addition)
-                    {
-                        worryLevel += operand;
-#if LOG_MONKEYS
-                    Logger.DebugLine($"\t\tWorry level increases by {operand} to {worryLevel}");
-#endif
-                    }
-                    else
-                    {
-                        worryLevel *= operand;
-#if LOG_MONKEYS
-                    Logger.DebugLine($"\t\tWorry level is multiplied by {operand} to {worryLevel}");
-#endif
-                    }
-                    worryLevel %= _divisor;
-                    // Note that worryLevel is _always_ a multiple of 3 here
-                    int target = _nextIndexFalse;
-                    if (worryLevel % _testDivisor == 0)
-                    {
-#if LOG_MONKEYS
-                    Logger.DebugLine($"\t\tCurrent worry level is divisible by {_testDivisor}");
-#endif
-                        target = _nextIndexTrue;
-                    }
-#if LOG_MONKEYS
-                else
-                {
-
-                    Logger.DebugLine($"\t\tCurrent worry level is not divisible by {_testDivisor}");
-                }
-                Logger.DebugLine($"\t\tItem with worry level {worryLevel} is thrown to monkey {target}");
-#endif
-                    monkeys[target]._items.Add(worryLevel);
-                }
-            }
-            _items.Clear();
-        }
-    }
-
-    internal class MonkeyBuilder2
-    {
-        private List<long> _startingItems = new();
-        private bool _addition;
-        private int _operationValue;
-        private int _testDivisor;
-        private int _nextIndexTrue;
-        private int _nextIndexFalse;
-        private int _divisor = 1;
-
-        public int Divisor => _divisor;
-
-        public void ParseStartingItems(string line)
-        {
-            var split = line[18..].Split(',');
-            foreach (var item in split)
-                _startingItems.Add(int.Parse(item));
-        }
-
-        public void ParseOperation(string line)
-        {
-            _addition = line[23] == '+';
-            var operand = line.AsSpan(25);
-            if (operand[0] == 'o')
-                _operationValue = -1;
-            else
-                _operationValue = int.Parse(operand);
-        }
-
-        public void ParseTest(string line, string trueLine, string falseLine)
-        {
-            _testDivisor = int.Parse(line.AsSpan(21));
-            _nextIndexTrue = int.Parse(trueLine.AsSpan(29));
-            _nextIndexFalse = int.Parse(falseLine.AsSpan(30));
-
-            _divisor *= _testDivisor;
-        }
-
-        public Monkey2 Build()
-        {
-            var monkey = new Monkey2(_startingItems, _addition, _operationValue, _testDivisor, _nextIndexTrue, _nextIndexFalse);
-            _startingItems = new List<long>();
-            _addition = false;
-            _operationValue = 0;
-            _testDivisor = 0;
-            _nextIndexTrue = 0;
-            _nextIndexFalse = 0;
-            return monkey;
         }
     }
 }
