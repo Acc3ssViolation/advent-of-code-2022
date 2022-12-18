b4573b30d02434cbc4f87de1865a290a5994aa64
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-18 19:21:21 +0100

    Day 17.1 WIP

--- Advent/Advent/Assignments/Day17_1.cs
+++ Advent/Advent/Assignments/Day17_1.cs
@@ -4,6 +4,13 @@
     {
         public string Run(IReadOnlyList<string> input)
         {
+            var width = 7;
+            var height = 10000;
+            var spawnHeight = 3;
+            var world = new byte[width * height];
+
+            // TODO: Fucking TETRIS
+
             return "";
         }
     }
--- Advent/Advent/Program.cs
+++ Advent/Advent/Program.cs
@@ -50,8 +50,10 @@ var interfaceType = typeof(IAssignment);
 //runner.Add(new Day14_2());
 //runner.Add(new Day15_1());
 //runner.Add(new Day15_2());
-runner.Add(new Day16_1());
-runner.Add(new Day16_2());
+//runner.Add(new Day16_1());
+//runner.Add(new Day16_2());
+runner.Add(new Day17_1());
+runner.Add(new Day17_2());
 #endif
 
 runner.LogTimingToFile = true;
