6d6afe440637538104b01bbb828aa3f6074fbf95
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-19 21:46:09 +0100

    Day 19.1 WIP
    Damn robots

---
+++ Advent/Advent/Assignments/Day19_1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Assignments
+{
+    internal class Day19_1 : IAssignment
+    {
+        public string Run(IReadOnlyList<string> input)
+        {
+            var qualitySum = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var blueprint = Blueprint.Parse(input[i]);
+                var quality = CalculateQualityLevel(blueprint, 24);
+                qualitySum += quality;
+            }
+
+            return qualitySum.ToString();
+        }
+
+        private int CalculateQualityLevel(Blueprint blueprint, int minutesLeft)
+        {
+            return blueprint.number * minutesLeft;
+        }
+
+        private class Blueprint
+        {
+            public int number;
+
+            public int oreRobotOreCost;
+
+            public int clayRobotOreCost;
+
+            public int obsidianRobotOreCost;
+            public int obsidianRobotClayCost;
+
+            public int geodeRobotOreCost;
+            public int geodeRobotObsidianCost;
+
+            public static Blueprint Parse(string input)
+            {
+                Blueprint blueprint = new();
+
+                var index = 10;
+                blueprint.number = ParseUtils.ParseIntPositive(input, ref index);
+
+                index += 23;
+                blueprint.oreRobotOreCost = ParseUtils.ParseIntPositive(input, ref index);
+
+                index += 28;
+                blueprint.clayRobotOreCost = ParseUtils.ParseIntPositive(input, ref index);
+
+                index += 32;
+                blueprint.obsidianRobotOreCost = ParseUtils.ParseIntPositive(input, ref index);
+                index += 9;
+                blueprint.obsidianRobotClayCost = ParseUtils.ParseIntPositive(input, ref index);
+
+                index += 30;
+                blueprint.geodeRobotOreCost = ParseUtils.ParseIntPositive(input, ref index);
+                index += 9;
+                blueprint.geodeRobotObsidianCost = ParseUtils.ParseIntPositive(input, ref index);
+
+                return blueprint;
+            }
+        }
+    }
+}
--- Advent/Advent/Program.cs
+++ Advent/Advent/Program.cs
@@ -52,10 +52,11 @@ var interfaceType = typeof(IAssignment);
 //runner.Add(new Day15_2());
 //runner.Add(new Day16_1());
 //runner.Add(new Day16_2());
-runner.Add(new Day17_1());
-runner.Add(new Day17_2());
+//runner.Add(new Day17_1());
+//runner.Add(new Day17_2());
 //runner.Add(new Day18_1());
 //runner.Add(new Day18_2());
+runner.Add(new Day19_1());
 #endif
 
 runner.LogTimingToFile = true;
