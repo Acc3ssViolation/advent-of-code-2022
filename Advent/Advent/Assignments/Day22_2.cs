3e5b09b0e987f239acdd1adf94f47965df9be7df
Acc3ss Violation <wrdbrookhuis@gmail.com>
2022-12-23 21:59:18 +0100

    Day 22.2 WIP

--- Advent/Advent/Assignments/Day22_2.cs
+++ Advent/Advent/Assignments/Day22_2.cs
@@ -12,14 +12,176 @@ namespace Advent.Assignments
             Left,
         }
 
+        /// <summary>
+        /// Cube sides in this pattern
+        ///     D
+        /// F B A C
+        ///     E
+        /// Opposite sides are AF, BC and DE
+        /// </summary>
+        private enum CubeSide
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F
+        }
+
+        private struct Cube
+        {
+            public CubeSide Face { get; private set; }
+            public CubeSide Back { get; private set; }
+            public CubeSide Up { get; private set; }
+            public CubeSide Down { get; private set; }
+            public CubeSide Left { get; private set; }
+            public CubeSide Right { get; private set; }
+
+            public CubeSide this[Direction dir] => dir switch
+            {
+                Direction.Right => Right,
+                Direction.Down => Down,
+                Direction.Left => Left,
+                _ => Up,
+            };
+
+            public Cube()
+            {
+                Face = CubeSide.A;
+                Back = CubeSide.F;
+                Up = CubeSide.D;
+                Down = CubeSide.E;
+                Left = CubeSide.B;
+                Right = CubeSide.C;
+            }
+
+            public static CubeSide Expected(CubeSide side, Direction dir) => dir switch
+            {
+                Direction.Right => ExpectedRight(side),
+                Direction.Down => ExpectedDown(side),
+                Direction.Left => ExpectedLeft(side),
+                _ => ExpectedUp(side),
+            };
+
+            public static CubeSide ExpectedUp(CubeSide side) => side switch
+            {
+                CubeSide.A => CubeSide.D,
+                CubeSide.B => CubeSide.D,
+                CubeSide.C => CubeSide.D,
+                CubeSide.F => CubeSide.D,
+                CubeSide.E => CubeSide.A,
+                _ => CubeSide.F,
+            };
+
+            public static CubeSide ExpectedDown(CubeSide side) => side switch
+            {
+                CubeSide.A => CubeSide.E,
+                CubeSide.B => CubeSide.E,
+                CubeSide.C => CubeSide.E,
+                CubeSide.F => CubeSide.E,
+                CubeSide.E => CubeSide.F,
+                _ => CubeSide.A,
+            };
+
+            public static CubeSide ExpectedRight(CubeSide side) => side switch
+            {
+                CubeSide.A => CubeSide.C,
+                CubeSide.B => CubeSide.A,
+                CubeSide.C => CubeSide.F,
+                CubeSide.F => CubeSide.B,
+                CubeSide.E => CubeSide.C,
+                _ => CubeSide.C,
+            };
+
+            public static CubeSide ExpectedLeft(CubeSide side) => side switch
+            {
+                CubeSide.A => CubeSide.B,
+                CubeSide.B => CubeSide.F,
+                CubeSide.C => CubeSide.A,
+                CubeSide.F => CubeSide.C,
+                CubeSide.E => CubeSide.B,
+                _ => CubeSide.B,
+            };
+
+            public static CubeSide Opposite(CubeSide side) => side switch
+            {
+                CubeSide.A => CubeSide.F,
+                CubeSide.B => CubeSide.C,
+                CubeSide.C => CubeSide.B,
+                CubeSide.F => CubeSide.A,
+                CubeSide.E => CubeSide.D,
+                _ => CubeSide.E,
+            }; 
+
+            public Cube RotateUp()
+            {
+                return new Cube
+                {
+                    Face = Up,
+                    Up = Back,
+                    Back = Down,
+                    Down = Face,
+                    Right = Right,
+                    Left = Left,
+                };
+            }
+
+            public Cube RotateDown()
+            {
+                return new Cube
+                {
+                    Face = Down,
+                    Up = Face,
+                    Back = Up,
+                    Down = Back,
+                    Right = Right,
+                    Left = Left,
+                };
+            }
+
+            public Cube RotateRight()
+            {
+                return new Cube
+                {
+                    Face = Right,
+                    Right = Back,
+                    Back = Left,
+                    Left = Face,
+                    Up = Up,
+                    Down = Down,
+                };
+            }
+
+            public Cube RotateLeft()
+            {
+                return new Cube
+                {
+                    Face = Left,
+                    Right = Face,
+                    Back = Right,
+                    Left = Back,
+                    Up = Up,
+                    Down = Down,
+                };
+            }
+
+            public override string ToString()
+            {
+                return Face.ToString();
+            }
+        }
+
         private class World<T>
         {
+            
+
             public int ChunkSize { get; }
             public int WorldSize { get; }
             public int WorldSizeInTiles { get; }
             public T[][] Chunks { get; }
 
-            private Dictionary<(int chunk, Rotation edge), (int chunk, RelativeRotation relative)> _wrapEdges = new();
+            private Dictionary<(int chunk, Direction edge), (int chunk, RelativeRotation relative)> _wrapEdges = new();
 
             public World(int chunkSize, int worldSizeInChunks)
             {
@@ -75,50 +237,206 @@ namespace Advent.Assignments
                 return true;
             }
 
+            private int ChunkUp(int index) => index - WorldSize;
+
+            private int ChunkDown(int index) => index + WorldSize;
+
+            private int ChunkLeft(int index) => index - 1;
+
+            private int ChunkRight(int index) => index + 1;
+
             public void ComputeWrappingTable()
             {
                 _wrapEdges.Clear();
 
-                for (var y = 0; y < WorldSize; y++)
-                {
-                    for (var x = 0; x < WorldSize; x++)
-                    {
-                        var chunk = Chunks[GetChunkIndex(x, y)];
-                        if (chunk == null) 
-                            continue;
+                var sidesToCubes = new Cube[6];
+                var sidesToIndexes = new int[6] { -1, -1, -1, -1, -1, -1 };
+                var sidesLeftToFind = 6;
+                var queue = new Queue<(Cube Cube, int Index)>();
 
-                        // Found a chunk
+                for (var index = 0; index < Chunks.Length; index++)
+                {
+                    if (Chunks[index] == null)
+                        continue;
+                    queue.Enqueue((new Cube(), index));
+                    break;
+                }
+
+                while (true)
+                {
+                    var current = queue.Dequeue();
+                    Logger.DebugLine($"Mapping side {current.Cube} to index {current.Index}");
+
+                    sidesToIndexes[(int)current.Cube.Face] = current.Index;
+                    sidesToCubes[(int)current.Cube.Face] = current.Cube;
+                    sidesLeftToFind--;
+                    if (sidesLeftToFind == 0)
+                        break;
+
+                    Cube side;
+                    side = current.Cube.RotateLeft();
+                    if (sidesToIndexes[(int)side.Face] < 0)
+                    {
+                        var index = ChunkLeft(current.Index);
+                        if (index >= 0 && index < Chunks.Length && Chunks[index] != null)
+                        {
+                            Logger.DebugLine($"{side} is left of {current.Cube}");
+                            queue.Enqueue((side, index));
+                        }
+                    }
+
+                    side = current.Cube.RotateRight();
+                    if (sidesToIndexes[(int)side.Face] < 0)
+                    {
+                        var index = ChunkRight(current.Index);
+                        if (index >= 0 && index < Chunks.Length && Chunks[index] != null)
+                        {
+                            Logger.DebugLine($"{side} is right of {current.Cube}");
+                            queue.Enqueue((side, index));
+                        }
+                    }
+
+                    side = current.Cube.RotateUp();
+                    if (sidesToIndexes[(int)side.Face] < 0)
+                    {
+                        var index = ChunkUp(current.Index);
+                        if (index >= 0 && index < Chunks.Length && Chunks[index] != null)
+                        {
+                            Logger.DebugLine($"{side} is up of {current.Cube}");
+                            queue.Enqueue((side, index));
+                        }
+                    }
+
+                    side = current.Cube.RotateDown();
+                    if (sidesToIndexes[(int)side.Face] < 0)
+                    {
+                        var index = ChunkDown(current.Index);
+                        if (index >= 0 && index < Chunks.Length && Chunks[index] != null)
+                        {
+                            Logger.DebugLine($"{side} is down of {current.Cube}");
+                            queue.Enqueue((side, index));
+                        }
                     }
                 }
 
+                int a = sidesToIndexes[(int)CubeSide.A];
+                int e = sidesToIndexes[(int)CubeSide.B];
+                int f = sidesToIndexes[(int)CubeSide.C];
+                int b = sidesToIndexes[(int)CubeSide.D];
+                int c = sidesToIndexes[(int)CubeSide.E];
+                int d = sidesToIndexes[(int)CubeSide.F];
 
+                // The 'normalized' rotations
+                // Look up as (side * 4 + direction)
+                var normalEdges = new int[] {
+                    // A (order is right, down, left, up)
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.None,
 
-                const int a = 2;
-                const int b = 6;
-                const int c = 10;
-                const int d = 5;
-                const int e = 11;
-                const int f = 4;
+                    // B
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Left,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Right,
 
-                _wrapEdges.Add((a, Rotation.Right), (e, RelativeRotation.Half));
-                _wrapEdges.Add((a, Rotation.Up), (f, RelativeRotation.Half));
-                _wrapEdges.Add((a, Rotation.Left), (d, RelativeRotation.Left));
+                    // C
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Right,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Left,
 
-                _wrapEdges.Add((b, Rotation.Right), (e, RelativeRotation.Right));
+                    // D
+                    (int) RelativeRotation.Right,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Left,
+                    (int) RelativeRotation.Half,
 
-                _wrapEdges.Add((c, Rotation.Down), (f, RelativeRotation.Half));
-                _wrapEdges.Add((c, Rotation.Left), (d, RelativeRotation.Right));
+                    // E
+                    (int) RelativeRotation.Left,
+                    (int) RelativeRotation.Half,
+                    (int) RelativeRotation.Right,
+                    (int) RelativeRotation.None,
 
-                _wrapEdges.Add((d, Rotation.Up), (a, RelativeRotation.Right));
-                _wrapEdges.Add((d, Rotation.Down), (c, RelativeRotation.Left));
+                    // F
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Half,
+                    (int) RelativeRotation.None,
+                    (int) RelativeRotation.Half,
+                };
 
-                _wrapEdges.Add((e, Rotation.Up), (b, RelativeRotation.Left));
-                _wrapEdges.Add((e, Rotation.Right), (a, RelativeRotation.Half));
-                _wrapEdges.Add((e, Rotation.Down), (f, RelativeRotation.Left));
+                var rotations = new int[6];
 
-                _wrapEdges.Add((f, Rotation.Up), (a, RelativeRotation.Half));
-                _wrapEdges.Add((f, Rotation.Down), (c, RelativeRotation.Half));
-                _wrapEdges.Add((f, Rotation.Left), (e, RelativeRotation.Right));
+                for (var side = 0; side < 6; side++)
+                {
+                    var actual = sidesToCubes[side][Direction.Up];
+                    var expected = Cube.ExpectedUp((CubeSide)side);
+
+                    if (expected == actual)
+                    {
+                        // Nothing
+                        rotations[side] = 0;
+                    }
+                    else if (expected == Cube.Opposite(actual))
+                    {
+                        rotations[side] = 2;
+                    }
+                    else
+                    {
+                        if (sidesToCubes[side][Direction.Left] == expected)
+                        {
+                            rotations[side] = 1;
+                        }
+                        else
+                        {
+                            rotations[side] = 3;
+                        }
+                    }
+                }
+
+                for (var side = 0; side < 6; side++)
+                {
+                    for (var dir = 0; dir < 4; dir++)
+                    {
+                        //var attachedSide = (int)sidesToCubes[side][(Direction)dir];
+                        var normalizedDir = (rotations[side] + dir) % 4;
+                        
+                        var attachedSide = (int)Cube.Expected((CubeSide)side, (Direction)normalizedDir);
+                        var normalRotation = normalEdges[side * 4 + normalizedDir];
+
+                        Logger.DebugLine($"Normal rotation from {(CubeSide)side} to {(CubeSide)attachedSide} is {(RelativeRotation)normalRotation}");
+
+                        var finalRotation = (normalRotation + rotations[attachedSide] + rotations[side]) % 4;
+
+                        Logger.DebugLine($"Final rotation from {(CubeSide)side} to {(CubeSide)attachedSide} is {(RelativeRotation)finalRotation}");
+                        Logger.Line();
+
+                        _wrapEdges.Add((sidesToIndexes[side], (Direction)dir), (sidesToIndexes[attachedSide], (RelativeRotation)finalRotation));
+                    }
+                }
+
+                Logger.Line();
+
+                //_wrapEdges.Add((a, Direction.Right), (c, RelativeRotation.Half));
+                //_wrapEdges.Add((a, Direction.Up), (d, RelativeRotation.Half));
+                //_wrapEdges.Add((a, Direction.Left), (b, RelativeRotation.Left));      Right
+
+                //_wrapEdges.Add((e, Direction.Right), (c, RelativeRotation.Right));    Right
+
+                //_wrapEdges.Add((f, Direction.Down), (d, RelativeRotation.Half));
+                //_wrapEdges.Add((f, Direction.Left), (b, RelativeRotation.Right));     Left
+
+                //_wrapEdges.Add((b, Direction.Up), (a, RelativeRotation.Right));       Half
+                //_wrapEdges.Add((b, Direction.Down), (f, RelativeRotation.Left));      Half
+
+                //_wrapEdges.Add((c, Direction.Up), (e, RelativeRotation.Left));
+                //_wrapEdges.Add((c, Direction.Right), (a, RelativeRotation.Half));
+                //_wrapEdges.Add((c, Direction.Down), (d, RelativeRotation.Left));
+
+                //_wrapEdges.Add((d, Direction.Up), (a, RelativeRotation.Half));
+                //_wrapEdges.Add((d, Direction.Down), (f, RelativeRotation.Half));
+                //_wrapEdges.Add((d, Direction.Left), (c, RelativeRotation.Right));
             }
 
             private (int X, int Y) GetChunkOrigin(int chunkIndex)
@@ -126,7 +444,7 @@ namespace Advent.Assignments
                 return ((chunkIndex % WorldSize) * ChunkSize, (chunkIndex / WorldSize) * ChunkSize);
             }
 
-            public (int X, int Y, Rotation Rotation) Wrap(int x, int y, int newX, int newY, Rotation rotation)
+            public (int X, int Y, Direction Rotation) Wrap(int x, int y, int newX, int newY, Direction rotation)
             {
                 var chunkIndex = GetChunkIndex(x, y);
                 var (newChunkIndex, relativeRotation) = _wrapEdges[(chunkIndex, rotation)];
@@ -144,7 +462,7 @@ namespace Advent.Assignments
                 else if (newY >= ChunkSize)
                     newY -= ChunkSize;
 
-                var newRotation = RotateRotation(rotation, relativeRotation);
+                var newRotation = RotateDirection(rotation, relativeRotation);
 
                 if (relativeRotation == RelativeRotation.None)
                 {
@@ -175,12 +493,12 @@ namespace Advent.Assignments
                 return (newX, newY, newRotation);
             }
 
-            public Rotation RotateRotation(Rotation rotation, RelativeRotation relativeRotation)
+            public Direction RotateDirection(Direction rotation, RelativeRotation relativeRotation)
             {
-                var angle = (int)rotation * 90;
-                var offset = (int)relativeRotation * 90;
+                var angle = (int)rotation;
+                var offset = (int)relativeRotation;
 
-                var result = (Rotation)(((angle + offset) % 360) / 90);
+                var result = (Direction)((angle + offset) % 4);
 
                 //Logger.DebugLine($"Rotating {rotation} {relativeRotation} gives {result}");
 
@@ -214,7 +532,7 @@ namespace Advent.Assignments
             }
         }
 
-        private enum Rotation
+        private enum Direction
         {
             Right,
             Down,
@@ -225,7 +543,7 @@ namespace Advent.Assignments
         private class Walker
         {
             public Vector2Int Position { get; set; }
-            public Rotation Rotation { get; set; }
+            public Direction Rotation { get; set; }
 
             private World<bool> _world;
 
@@ -263,9 +581,9 @@ namespace Advent.Assignments
                 {
                     var delta = Rotation switch
                     {
-                        Rotation.Left => new Vector2Int(-1, 0),
-                        Rotation.Right => new Vector2Int(1, 0),
-                        Rotation.Up => new Vector2Int(0, -1),
+                        Direction.Left => new Vector2Int(-1, 0),
+                        Direction.Right => new Vector2Int(1, 0),
+                        Direction.Up => new Vector2Int(0, -1),
                         _ => new Vector2Int(0, 1),
                     };
 
@@ -297,16 +615,16 @@ namespace Advent.Assignments
 
             public void RotateLeft()
             {
-                if (Rotation == Rotation.Right)
-                    Rotation = Rotation.Up;
+                if (Rotation == Direction.Right)
+                    Rotation = Direction.Up;
                 else
                     Rotation--;
             }
 
             public void RotateRight()
             {
-                if (Rotation == Rotation.Up)
-                    Rotation = Rotation.Right;
+                if (Rotation == Direction.Up)
+                    Rotation = Direction.Right;
                 else
                     Rotation++;
             }
